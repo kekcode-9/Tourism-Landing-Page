1b9ae4e3256c0fcd42009102355d022a8a3d28dd
kekcode-9 <rupamita.s97@gmail.com>
2024-09-11 13:02:32 +0530

    test

--- components/landing-page-places.tsx
+++ components/landing-page-places.tsx
@@ -261,11 +261,11 @@ export default function Places() {
     }
     let newPlaceIndex;
     let scrollUp = false;
-    if (currentTouchY < lastTouchY && (lastTouchY - currentTouchY) > 16) {
+    if (currentTouchY < lastTouchY && (lastTouchY - currentTouchY) > 100) {
       newPlaceIndex = currPlaceIndex + 1;
       setTestMessage('scrolling down to index: ' + newPlaceIndex);
       scrollUp = false;
-    } else if (currentTouchY > lastTouchY && (currentTouchY - lastTouchY) > 16) {
+    } else if (currentTouchY > lastTouchY && (currentTouchY - lastTouchY) > 100) {
       newPlaceIndex = currPlaceIndex - 1;
       setTestMessage('scrolling up to index: ' + newPlaceIndex);
       scrollUp = true;
